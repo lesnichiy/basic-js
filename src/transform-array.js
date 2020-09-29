c1194bc53bb20722e73290b4dc364da88009e923
lesnichiy <lesnichiy02@ya.ru>
2020-09-29 22:21:42 +0300

    feat: add solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform( arr ) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if ( !Array.isArray(arr) ) {
+    throw new Error('Parameter is not an Array!');
+  } else {
+    return arr.reduce( (resultArr, item, i, sourceArr) => {
+      if ((sourceArr[i - 1] === '--discard-next')
+            || (sourceArr[i + 1] === '--discard-prev')
+            || item === '--discard-next'
+            || item === '--discard-prev'
+      ) {
+        return resultArr;
+      } else if (item === '--double-next'
+                  && sourceArr[i + 1] !== undefined
+      ) {
+        return [ ...resultArr, sourceArr[i + 1] ];
+      } else if (item === '--double-prev'
+                  && sourceArr[i - 1] !== undefined
+                  && sourceArr[i - 2] !== '--discard-next'
+      ) {
+        return [ ...resultArr, sourceArr[i - 1] ];
+      } else if (item === '--double-next'
+                  || item === '--double-prev'
+      ) {
+        return resultArr;
+      } else {
+        return [ ...resultArr, item];
+      }
+    }, []);
+  }
 };
